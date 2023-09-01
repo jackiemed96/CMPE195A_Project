a9d32c454bb711a6e2872a6c5dcf2277365dfd8d
MendozaVer <veronica.mendoza-cardenas@sjsu.edu>
2023-09-01 14:38:40 -0700

    database updates

--- smart_planter/DHT22.py
+++ smart_planter/DHT22.py
@@ -1,16 +1,19 @@
-#from planter import db, create_app
-from planter import db, app
-from planter.models import WeatherData
-import socket, time
+import socket
+import time
 
-#app = create_app()
+from planter import app
+# import db from models to aboid circular import error
+from planter.models import WeatherData, db
+
 bufferSize = 1024
 
-serverAddress = ("192.168.0.90", 2223) #(IP, PORT); IP may vary
+# we may consider getting the IP and PORT from environment variables if they vary
+# in the future we will implement reading from .env using python_dotnev module
+serverAddress = ("192.168.0.90", 2223)  # (IP, PORT); IP may vary
 
 UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while (True):
+while True:
     cmd = "GO"
     cmd = cmd.encode("utf-8")
     UDPClient.sendto(cmd, serverAddress)
@@ -18,18 +21,18 @@ while (True):
 
     data = data.decode("utf-8")
     dataArray = data.split(":")
-    
+
     with app.app_context():
         db.create_all()
         dataArray[0] = (float(dataArray[0]) * 1.8) + 32
-        newSensData = WeatherData(temp = dataArray[0], humidity = dataArray[1])
+        newSensData = WeatherData(temp=dataArray[0], humidity=dataArray[1])
         db.session.add(newSensData)
         db.session.commit()
 
-    if (len(dataArray) == 1):
+    if len(dataArray) == 1:
         print("No data")
-    
-    if (len(dataArray) == 2):
+
+    if len(dataArray) == 2:
         print("Temperature: ", dataArray[0], " Humidity: ", dataArray[1])
 
     time.sleep(3)
